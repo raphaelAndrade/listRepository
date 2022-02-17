12ad95960dbd3117f3a13141536315c0139fe1da
Raphael <raphaelsantosdesigner@gmail.com>
2022-02-17 11:33:07 -0800

    finish function to list repository

--- script.js
+++ script.js
@@ -2,11 +2,31 @@
 
 const listRepository = () => {
     const user = document.getElementById('userID').value;
+    // const url = "https://api.github.com/users/VikiSevcikova/repos?"
+    const url = `https://api.github.com/users/${user}/repos`;
     if(user === ""){
         alert("The input is required. Please insert a valid User Name")
     } else {
-        console.log(user);
-        return user;
+       fetch(url).then(resp => {
+           const status = resp.status;
+           if(status !== 200){
+               console.log(`Ops! Something is wrong ${status}`)
+           }
+           resp.json().then(repository => {
+               console.log(repository)
+               repository.map(value => {
+                   const name = value.name;
+                   const url = value.hooks_url;
+               })
+               /*
+                0) Clone the repository and create your own branch before start to work
+                1) Print the name of the repository
+                2) Print the link of the repository
+                3) One or two more information that you believe its good for your application
+                4) Create a pull request when you finish your work
+               */
+           })
+       })
     }
 }
 
