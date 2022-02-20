9cbc9045528ecc039a93e26820872021de1c6bb7
unknown <you@example.com>
2022-02-19 23:38:41 -0800

    commit 2

--- script.js
+++ script.js
@@ -2,129 +2,64 @@
 
 const listRepository = () => {
     const user = document.getElementById('userID').value;
-    // const url = "https://api.github.com/users/VikiSevcikova/repos?"
     const url = `https://api.github.com/users/${user}/repos`;
-    if(user === ""){
+    if (user === "") {
         alert("The input is required. Please insert a valid User Name")
     } else {
-       fetch(url).then(resp => {
-           const status = resp.status;
-           if(status !== 200){
-               console.log(`Ops! Something is wrong ${status}`)
-           }
-           resp.json().then(repository => {
-               console.log(repository)
-               repository.map(value => {
-                   const name = value.name;
-                   const url = value.hooks_url;
-               })
-               /*
-                0) Clone the repository and create your own branch before start to work
-                1) Print the name of the repository
-                2) Print the link of the repository
-                3) One or two more information that you believe its good for your application
-                4) Create a pull request when you finish your work
-               */
-           })
-       })
+        fetch(url).then(resp => {
+            const status = resp.status;
+            if (status !== 200) {
+                console.log(`Ops! Something is wrong ${status}`)
+            }
+            resp.json().then(repository => {
+                console.log(repository);
+                const repoListBodyElement = document.getElementsByClassName('listRepo')[0];
+                repoListBodyElement.replaceChildren();
+                let rowNumber = 1;
+                repository.map(value => {
+                    const name = value.name;
+                    const url = value.html_url;
+                    const language = value.language;
+                    const wiki = value.has_wiki;
+                    const forks = value.forks;
+
+                    const repoRowElement = document.createElement('tr');
+
+                    const repoRowNumberCell = document.createElement('th');
+                    repoRowNumberCell.attributes['scope'] = 'row';
+                    repoRowNumberCell.innerText = rowNumber++;
+                    repoRowElement.appendChild(repoRowNumberCell);
+
+                    const repoNameCell = document.createElement('td');
+                    repoNameCell.innerText = name;
+                    repoRowElement.appendChild(repoNameCell);
+
+                    const repoUrlCell = document.createElement('td');
+                    repoUrlCell.innerText = url;
+                    repoRowElement.appendChild(repoUrlCell);
+
+                    const repoLanguageCell = document.createElement('td');
+                    repoLanguageCell.innerText = language;
+                    repoRowElement.appendChild(repoLanguageCell);
+
+                    const repoWikiCell = document.createElement('td');
+                    repoWikiCell.innerHTML = wiki ? '&#10003' : '';
+                    repoRowElement.appendChild(repoWikiCell);
+
+                    const repoForksCell = document.createElement('td');
+                    repoForksCell.innerText = forks;
+                    repoRowElement.appendChild(repoForksCell);
+
+                    repoListBodyElement.appendChild(repoRowElement);
+                })
+            })
+        })
     }
 }
 
 //Add event click to the button
 const button = document.getElementById('searchRepository');
-button.addEventListener('click',(e)=> {
+button.addEventListener('click', (e) => {
     e.preventDefault();
     listRepository();
 })
-
-/*----------------------------------------------------------------- */
-
-// SELECTING ALL TEXT ELEMENTS
-var username = document.forms['vform']['username'];
-var email = document.forms['vform']['email'];
-var password = document.forms['vform']['password'];
-var password_confirm = document.forms['vform']['password_confirm'];
-// SELECTING ALL ERROR DISPLAY ELEMENTS
-var name_error = document.getElementById('name_error');
-var email_error = document.getElementById('email_error');
-var password_error = document.getElementById('password_error');
-// SETTING ALL EVENT LISTENERS
-username.addEventListener('blur', nameVerify, true);
-email.addEventListener('blur', emailVerify, true);
-password.addEventListener('blur', passwordVerify, true);
-// validation function
-function Validate() {
-  // validate username
-  if (username.value == "") {
-    username.style.border = "1px solid red";
-    document.getElementById('username_div').style.color = "red";
-    name_error.textContent = "Username is required";
-    username.focus();
-    return false;
-  }
-  // validate username
-  if (username.value.length < 3) {
-    username.style.border = "1px solid red";
-    document.getElementById('username_div').style.color = "red";
-    name_error.textContent = "Username must be at least 3 characters";
-    username.focus();
-    return false;
-  }
-  // validate email
-  if (email.value == "") {
-    email.style.border = "1px solid red";
-    document.getElementById('email_div').style.color = "red";
-    email_error.textContent = "Email is required";
-    email.focus();
-    return false;
-  }
-  // validate password
-  if (password.value == "") {
-    password.style.border = "1px solid red";
-    document.getElementById('password_div').style.color = "red";
-    password_confirm.style.border = "1px solid red";
-    password_error.textContent = "Password is required";
-    password.focus();
-    return false;
-  }
-  // check if the two passwords match
-  if (password.value != password_confirm.value) {
-    password.style.border = "1px solid red";
-    document.getElementById('pass_confirm_div').style.color = "red";
-    password_confirm.style.border = "1px solid red";
-    password_error.innerHTML = "The two passwords do not match";
-    return false;
-  }
-}
-// event handler functions
-function nameVerify() {
-  if (username.value != "") {
-   username.style.border = "1px solid #5e6e66";
-   document.getElementById('username_div').style.color = "#5e6e66";
-   name_error.innerHTML = "";
-   return true;
-  }
-}
-function emailVerify() {
-  if (email.value != "") {
-  	email.style.border = "1px solid #5e6e66";
-  	document.getElementById('email_div').style.color = "#5e6e66";
-  	email_error.innerHTML = "";
-  	return true;
-  }
-}
-function passwordVerify() {
-  if (password.value != "") {
-  	password.style.border = "1px solid #5e6e66";
-  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
-  	document.getElementById('password_div').style.color = "#5e6e66";
-  	password_error.innerHTML = "";
-  	return true;
-  }
-  if (password.value === password_confirm.value) {
-  	password.style.border = "1px solid #5e6e66";
-  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
-  	password_error.innerHTML = "";
-  	return true;
-  }
-}
